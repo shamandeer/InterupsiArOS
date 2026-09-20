e85228f51f6240bdae5e06288f485adce2e6affe
rafipradiptaas <rafi.sulistyo@gmail.com>
2026-09-20 22:21:17 +0700

    feat: added gdt.c skeleton

---
+++ src/cpu/gdt.c
@@ -0,0 +1,30 @@
+#include "header/cpu/gdt.h"
+
+/**
+ * global_descriptor_table, predefined GDT.
+ * Initial SegmentDescriptor already set properly according to Intel Manual & OSDev.
+ * Table entry : [{Null Descriptor}, {Kernel Code}, {Kernel Data (variable, etc)}, ...].
+ */
+struct GlobalDescriptorTable global_descriptor_table = {
+    .table = {
+        {
+            // TODO : Implement
+        },
+        {
+            // TODO : Implement
+        },
+        {
+            // TODO : Implement
+        }
+    }
+};
+
+/**
+ * _gdt_gdtr, predefined system GDTR. 
+ * GDT pointed by this variable is already set to point global_descriptor_table above.
+ * From: https://wiki.osdev.org/Global_Descriptor_Table, GDTR.size is GDT size minus 1.
+ */
+struct GDTR _gdt_gdtr = {
+    // TODO : Implement, this GDTR will point to global_descriptor_table. 
+    //        Use sizeof operator
+};
